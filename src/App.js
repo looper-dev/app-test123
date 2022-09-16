d355fbf338323336aa556c59d3b78e39b812be62
Loopp <113578080+looper-dev@users.noreply.github.com>
2022-09-17 02:45:54 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>เริ่มต้น</code> การพัฒนา react.
+          เริ่มต้น <code>สร้าง และ</code> การพัฒนา react.
         </p>
         <a
           className="App-link"
