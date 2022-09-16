5a9098b73a78d64d832f3388288c1e970de4a0ee
Loopp <113578080+looper-dev@users.noreply.github.com>
2022-09-17 02:44:19 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>เริ่มต้น</code> การพัฒนา react.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://app-test123.vercel.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ทดสอบ
         </a>
       </header>
     </div>
