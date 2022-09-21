2bc5d65b9f6caeaeaf0d57a87b8eb29f6a89fe46
Loopp <admin@looperdev.com>
2022-09-22 01:22:16 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        เครดิตฟรีล่าสุด <code>แจกไม่อั้น กดรับได้ทันที</code> ไม่ต้องรอนาน.
+        เครดิตฟรีล่าสุด<code>แจกไม่อั้น</code>กดรับได้ทันที.
         </p>
         <a
           className="App-link"
